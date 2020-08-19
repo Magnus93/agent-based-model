745225c7df43cb35bf674ef776cb863d7d3e551e
Unknown <magnus.ja.gustafsson@gmail.com>
2020-08-19 11:11:48 +0200

    Change method name move_individual -> move_agent

--- agents.py
+++ agents.py
@@ -48,7 +48,7 @@ class Agents(object):
             agent["time_until"] = erlang(3, agent["Ti"]/3) 
 
 
-    def move_individual(self, agent, dst_stage):
+    def move_agent(self, agent, dst_stage):
         src_stage = agent["stage"] 
         self.agents[src_stage].remove(agent)
         self.agents[dst_stage].append(agent)
@@ -61,13 +61,13 @@ class Agents(object):
         for agent in self.agents["I"]:
             agent["time_until"] -= timestep
             if (agent["time_until"] < 0):
-                self.move_individual(agent, "R") 
+                self.move_agent(agent, "R") 
 
         # loop through exposed agents 
         for agent in self.agents["E"]:
             agent["time_until"] -= timestep
             if (agent["time_until"] < 0):
-                self.move_individual(agent, "I") 
+                self.move_agent(agent, "I") 
 
         # loop through suseptible and check their if any gets infected 
         for agent in self.agents["S"]:
@@ -75,7 +75,7 @@ class Agents(object):
             # dt*NI*ln(1-p) 
             risk = 1 - math.exp(timestep * NI * math.log(1-agent["p"]))
             if uniform < risk:
-                self.move_individual(agent, "E")
+                self.move_agent(agent, "E")
 
     def get_num(self, stage, group_name=None):
         if group_name == None:
