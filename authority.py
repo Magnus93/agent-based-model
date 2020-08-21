7bedc43922057cad8d405292d14d604c13470854
Unknown <magnus.ja.gustafsson@gmail.com>
2020-08-21 14:54:10 +0200

    Fixed variable bug

--- authority.py
+++ authority.py
@@ -18,7 +18,7 @@ class Authority:
         if ( 7-self.delay <= time%7 < 7-self.delay+timestep ):
             # print("authority esitmation day: {}".format(time%7))
             sigma = 0.25
-            bias = max(0, random.normalvariate(1, 0.25))
+            bias = max(0, random.normalvariate(1, sigma))
             self.est_infectious = int(I*bias) 
             if (self.est_infectious > 0.1*self.pop_size):
                 self.p_factor = 0.5 
