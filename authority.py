afa52a7f5eae7b8e65ac8fea6b42535206ce6128
Unknown <magnus.ja.gustafsson@gmail.com>
2020-08-24 16:13:01 +0200

    Changed behavior of authority to match Conceptual Model

--- authority.py
+++ authority.py
@@ -2,30 +2,33 @@ import random
 
 class Authority:
     def __init__(self, population_size):
-        self.p_factor = 1 
         # estimated infectious 
         self.est_infectious = 0 
         # poulation size 
         self.pop_size = population_size 
-        # delay 
-        self.delay = 7  
-
+        
+        # percent limit of infectious in the population 
+        # if the limit is past then preventive measures start 
+        self.limit = 0.04
+        self.preventive_measures = False 
 
     def step(self, time, timestep, I):
+        # esitmate number of infectious 
         if (time%7 < timestep):
-            self.delay = random.triangular(3, 7, 5)
-
-        if ( 7-self.delay <= time%7 < 7-self.delay+timestep ):
-            # print("authority esitmation day: {}".format(time%7))
             sigma = 0.25
             bias = max(0, random.normalvariate(1, sigma))
-            self.est_infectious = int(I*bias) 
-            if (self.est_infectious > 0.1*self.pop_size):
-                self.p_factor = 0.5 
-            else:
-                self.p_factor = 1 
+            self.est_infectious = I*bias 
 
+        if (not self.preventive_measures):
+            delay = random.triangular(3, 7, 5)
+            # 3-7 days to process estimation and take preventive measures 
+            if ( delay <= time%7 < delay + timestep):
+                if (self.est_infectious > self.limit * self.pop_size):
+                    self.preventive_measures = True 
 
     def get_p_factor(self):
-        return self.p_factor 
+        if (self.preventive_measures):
+            return 0.5
+        else:
+            return 1
     
