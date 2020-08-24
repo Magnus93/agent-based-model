794b883779722d7eb8ba74732f1d0b3d7ea4ef32
Unknown <magnus.ja.gustafsson@gmail.com>
2020-08-24 16:44:45 +0200

    Fixed bug

--- agent.py
+++ agent.py
@@ -41,7 +41,7 @@ class Agent:
     def step(self, time, timestep, NI, p_factor, authority):
         if (self.stage == "S"):
             uniform = random.random()
-            p = self.p * p_factor * authority.p_factor
+            p = self.p * p_factor * authority.get_p_factor() 
             risk = 1 - math.exp(timestep * NI * math.log(1-p))
             if uniform < risk:
                 self.set_stage("E", time)  
